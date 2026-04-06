773724608ae8bcd593ad069af98ba23804328ad9
itzhan <2802965114@qq.com>
2026-04-07 07:29:18 +0800

    init4

--- sql/zz_alter_refund_auditstatus.sql
+++ sql/zz_alter_refund_auditstatus.sql
@@ -1,5 +1,4 @@
 -- 退款审核状态字段迁移
-ALTER TABLE courseenrollment ADD COLUMN auditstatus VARCHAR(20) DEFAULT '待审核' COMMENT '审核状态';
 ALTER TABLE refundcourseenrollment ADD COLUMN auditstatus VARCHAR(20) DEFAULT '待审核' COMMENT '审核状态';
 ALTER TABLE refundproductorder ADD COLUMN auditstatus VARCHAR(20) DEFAULT '待审核' COMMENT '审核状态';
 ALTER TABLE refundcardapplication ADD COLUMN auditstatus VARCHAR(20) DEFAULT '待审核' COMMENT '审核状态';
